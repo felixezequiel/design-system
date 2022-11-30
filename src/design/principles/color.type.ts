bbd7c83d2c17cc8ce548d985eeeb14df14ba2e47
felix <felix@yampa.com.br>
2022-11-30 10:01:48 -0300

    Refactory types

--- src/design/principles/color.type.ts
+++ src/design/principles/color.type.ts
@@ -1,37 +1,74 @@
-export interface VariantColor {
-  DEFAULT: string;
-  100: string;
-  400: string;
-  700: string;
-}
+export type VariantPrimary = "primary";
+export type VariantSecondary = "secondary";
+export type VariantPremium = "premium";
+export type VariantHighlight = "highlight";
+export type VariantDEFAULT = "DEFAULT";
+export type VariantLow = "low";
+export type VariantHigh = "high";
+export type VariantAlert = "alert";
+export type VariantSuccess = "success";
 
-export interface DefaultColor {
-  low: VariantColor;
-  high: VariantColor;
-}
+export type Intencity100 = 100;
+export type Intencity400 = 400;
+export type Intencity700 = 700;
 
-export interface BrandColor {
-  primary: VariantColor;
-  premium: VariantColor;
-  secondary: VariantColor;
-  highlight: VariantColor;
-}
+export type KeyIntencity =
+  | VariantDEFAULT
+  | Intencity100
+  | Intencity400
+  | Intencity700;
 
-export interface FeedbackColor {
-  alert: VariantColor;
-  success: VariantColor;
-}
+export type Intencity = {
+  [key in KeyIntencity]: string;
+};
 
-export interface GradientColors {
-  secondary: string;
-  primary: string;
-  premium: string;
-  DEFAULT: string;
-}
+export type KeyDefaultColor = VariantLow | VariantHigh;
 
-export interface Colors {
-  default: DefaultColor;
-  brand: BrandColor;
-  feedback: FeedbackColor;
-  gradient: GradientColors;
-}
+export type DefaultColor = {
+  [key in KeyDefaultColor]: Intencity;
+};
+
+export type KeyBrandColor =
+  | VariantPrimary
+  | VariantPremium
+  | VariantSecondary
+  | VariantHighlight;
+export type BrandColor = {
+  [key in KeyBrandColor]: Intencity;
+};
+
+export type KeyFeedbackColor = VariantAlert | VariantSuccess;
+export type FeedbackColor = {
+  [key in KeyFeedbackColor]: Intencity;
+};
+
+export type KeyGradientColor =
+  | VariantPrimary
+  | VariantSecondary
+  | VariantPremium
+  | VariantDEFAULT;
+
+export type GradientColors = {
+  [key in KeyGradientColor]: string;
+};
+
+export type Default = "default";
+export type Brand = "brand";
+export type Feedback = "feedback";
+export type Gradient = "gradient";
+
+export type Principles = Default | Brand | Feedback | Gradient;
+
+export type ValuesColors<K> = K extends Default
+  ? DefaultColor
+  : K extends Brand
+  ? BrandColor
+  : K extends Feedback
+  ? FeedbackColor
+  : K extends Gradient
+  ? GradientColors
+  : undefined;
+
+export type Colors = {
+  [key in Principles]: ValuesColors<key>;
+};
